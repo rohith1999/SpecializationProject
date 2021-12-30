bc055deb1d10bd19ec7b203638d4f11639509b47
Tasmiya-Km <tasmiyakm.ckm@gmail.com>
2021-12-30 19:40:59 +0530

    tasmiya

--- src/main/java/com/th/constants/PropertyConstant.java
+++ src/main/java/com/th/constants/PropertyConstant.java
@@ -1,7 +1,7 @@
 package com.th.constants;
 
 public class PropertyConstant {
-	public static final String AUTH_USER = "/auth";
+//	public static final String AUTH_USER = "/auth";
 	public static final String REGISTER_USER = "/register";
 
 }
